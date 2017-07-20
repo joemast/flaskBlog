1d628eb3db0d083e52bc1e99118c8ad1222ddd1c
Marcelo Marcon <user@local.index>
2017-07-19 23:55:55 -0400

    Merge Conflict

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from os import path
 
 from flask import (Flask, flash, g, redirect, render_template, request,
-                   session, url_for)
+                   session, url_for, Blueprint)
 from flask_login import (LoginManager, current_user, login_required,
                          login_user, logout_user)
 from flaskext.markdown import Markdown
@@ -17,12 +17,24 @@ login_manager.init_app(app)
 Markdown(app)
 login_manager.login_view = "login"
 db.init_app(app)
+Markdown(app)
 
 
+# Filters
 @app.template_filter('truncate_chars')
 def truncate_chars(s):
     return s[:500]
 
+
+@app.template_filter('format_date')
+def format_date(date, fmt=None):
+    if fmt:
+        return date.strftime(fmt)
+    else:
+        return date.strftime("%a, %d %b %Y")
+
+
+# Context Managers
 @login_manager.user_loader
 def user_loader(user_id):
     return Login.query.get(int(user_id))
@@ -33,13 +45,98 @@ def before_request():
     db.create_all()
 
 
+# Error Handlers
 @app.errorhandler(404)
 def errorhandler(e):
-    return redirect(url_for("login"))
+    return redirect(url_for("auth.login"))
 
 
-@app.route("/sign", methods=["GET", "POST"])
-def sign():
+# Blueprints
+blog = Blueprint('blog', __name__)
+auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+# Routes
+@blog.route("/new_post", methods=["POST", "GET"])
+@login_required
+def new_post():
+    if request.method == "POST":
+        fname = request.form["name"]
+        fcontent = request.form["content"]
+        post = Post(title=fname, content=fcontent, login_id=current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for("blog.my_posts"))
+    return render_template("blog/new_post.html")
+
+
+@blog.route("/my_posts", methods=["POST", "GET"])
+@login_required
+def my_posts():
+    posts = Post.query.filter_by(login_id=current_user.id)
+    return render_template("blog/my_posts.html", posts=posts)
+
+
+@blog.route("/")
+def index():
+    posts = Post.query.all()
+    return render_template("blog/index.html", posts=posts)
+
+
+@blog.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get(post_id)
+    return render_template("blog/post.html", post=post)
+
+
+@blog.route("/edit_post/<int:post_id>", methods=["POST", "GET"])
+@login_required
+def edit_post(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    if request.method == "POST" and post.login_id == current_user.id:
+        post.title = request.form["title"]
+        post.content = request.form["content"]
+        post.pub_date = datetime.utcnow()
+        db.session.commit()
+        return redirect(url_for("blog.my_posts"))
+    return render_template("blog/edit_post.html", post=post)
+
+
+@blog.route("/delete_post/<int:post_id>")
+@login_required
+def delete_post(post_id):
+    post = Post.query.get(post_id)
+    if post.login_id != current_user.id:
+        flash("voce não tem permissão para excluir este post.")
+        return redirect(url_for('blog.index'))
+    db.session.query(Post).filter_by(id=post_id).delete()
+    db.session.commit()
+    return redirect(url_for("blog.my_posts"))
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You are now Logged Out !")
+    return redirect(url_for("blog.index"))
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        log_user = Login.query.filter_by(username=request.form["username"]).first()
+        if log_user and chpass(log_user.password, request.form["password"]):
+            login_user(log_user)
+            flash("You are Logged")
+            return redirect(url_for("blog.index"))
+        else:
+            flash("Username or password are wrong.")
+    return render_template("auth/login.html")
+
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
     if request.method == "POST":
         if not request.form["username"]:
             flash("Enter with a Username")
@@ -51,102 +148,20 @@ def sign():
             flash("Enter with a Password")
         else:
             try:
-                user = Login(request.form["username"],
-                             request.form["email"],
-                             request.form["name"],
-                             request.form["password"])
+                user = Login(request.form["username"], request.form["email"],
+                             request.form["name"], request.form["password"])
                 db.session.add(user)
                 db.session.commit()
                 flash("User Sign")
-                return redirect(url_for("login"))
+                return redirect(url_for("auth.login"))
             except exc.IntegrityError:
                 flash("This Username Or Email Alredy Exists")
-                return redirect(url_for("login"))
-
-    return render_template("sign.html")
-
-
-@app.route("/", methods=["POST", "GET"])
-@login_required
-def index():
-    if request.method == "POST":
-        fname = request.form["name"]
-        fcontent = request.form["content"]
-
-        post = Post(title=fname, content=fcontent, login_id=current_user.id)
-        db.session.add(post)
-        db.session.commit()
-        return redirect(url_for("result"))
-
-    return render_template("index.html")
+                return redirect(url_for("auth.login"))
+    return render_template("auth/register.html")
 
 
-@app.route("/result", methods=["POST", "GET"])
-@login_required
-def result():
-    post = Post.query.filter_by(login_id=current_user.id)
-    return render_template("result.html", result=post,
-                            user=current_user.username)
-
-
-@app.route("/public", methods=["POST", "GET"])
-def public():
-    post = Post.query.all()
-    return render_template("public.html", result=post)
-
-
-@app.route("/public/<int:post_id>", methods=["POST", "GET"])
-def post(post_id):
-    post = Post.query.filter_by(id=post_id).first()
-    return render_template("post.html", result=post)
-
-
-@app.route("/update/<int:uid>", methods=["POST", "GET"])
-@login_required
-def update(uid):
-    post = Post.query.filter_by(id=uid).first()
-    # user_id = User.query.filter_by(id=uid).first()
-    if request.method == "POST":
-        post.title = request.form["title"]
-        post.content = request.form["content"]
-        post.pub_date = datetime.utcnow()
-        db.session.commit()
-        return redirect(url_for("result"))
-
-    return render_template("update.html", post=post)
-
-
-@app.route("/delete/<int:uid>")
-@login_required
-def delete(uid):
-    db.session.query(Post).filter_by(id=uid).delete()
-    db.session.commit()
-    return redirect(url_for("result"))
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    flash("You are now Logged Out !")
-    return redirect(url_for("login"))
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        log_user = Login.query.filter_by(username=request.form["username"]).first()
-        if log_user:
-            if log_user.username == request.form["username"] and (
-            chpass(log_user.password, request.form["password"]) == True):
-                login_user(log_user)
-                flash("You are Logged")
-                return redirect(url_for("index"))
-
-        return redirect(url_for("login"))
-
-    return render_template("login.html")
-
+app.register_blueprint(blog)
+app.register_blueprint(auth)
 
 if __name__ == '__main__':
     app.run()
